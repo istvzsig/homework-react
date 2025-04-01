5694c9afd26edbfae864b1e3e6769cc4158868c8
istvzsig <istvanzsigmond@aol.com>
2025-04-01 20:57:42 +0200

    app config

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      // Exclude test files from being processed
+      config.module.rules.push({
+        test: /\.test\.js$/,
+        use: "ignore-loader",
+      });
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
