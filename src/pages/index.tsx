935e9fe7dfdf7bfb424d314a834c3653b0412992
istvzsig <istvanzsigmond@aol.com>
2025-04-01 20:07:22 +0200

    update index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,11 @@
-// pages/index.tsx
 import React from "react";
 
 import ProductsOrganizer from "./components/products/ProductsOrganizer";
-// import UsersOrganizer from "./components/users/UsersOrganizer";
 
 const Home: React.FC = () => {
   return (
     <div>
       <ProductsOrganizer />
-      {/* <UsersOrganizer /> */}
     </div>
   );
 };
