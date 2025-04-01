b4f5121c108a52d65e889a5a32926fe4de42a22b
istvzsig <istvanzsigmond@aol.com>
2025-04-01 17:23:08 +0200

    delete unused babel config

--- babel.config.js
+++
@@ -1,8 +0,0 @@
-// babel.config.js
-module.exports = {
-  presets: [
-    "@babel/preset-env", // For ESNext features
-    "@babel/preset-react", // For React
-    "@babel/preset-typescript", // If using TypeScript
-  ],
-};
