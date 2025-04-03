99d5613324e46958f0ca83b57820ad065fc123c3
istvzsig <istvanzsigmond@aol.com>
2025-04-03 17:18:48 +0200

    introduce users reducers

---
+++ src/reducers/users.reducer.tsx
@@ -0,0 +1,28 @@
+import { User } from "@/models/user.model";
+
+const genderCountReducer = (users: User[]) => {
+  return {
+    male: users.filter((u) => u.gender === "male").length,
+    female: users.filter((u) => u.gender === "female").length,
+  };
+};
+
+const hairColorsCountReducer = (users: User[]) => {
+  return users.reduce((acc, user) => {
+    acc[user.hair.color] = (acc[user.hair.color] || 0) + 1;
+    return acc;
+  }, {} as Record<User["hair"]["color"], number>);
+};
+
+const mapAddressesToPostalCodeReducer = (users: User[]) => {
+  return users.reduce((addrAcc, u) => {
+    addrAcc[u.firstName + u.lastName] = u.address.postalCode;
+    return addrAcc;
+  }, {} as Record<string, User["address"]["postalCode"]>);
+};
+
+export {
+  genderCountReducer,
+  hairColorsCountReducer,
+  mapAddressesToPostalCodeReducer,
+};
