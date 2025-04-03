aaf7a9b184f7401d5b6d9b4a100024d876b01247
istvzsig <istvanzsigmond@aol.com>
2025-04-03 21:54:34 +0200

    update

--- src/components/users/UsersOrganizer.tsx
+++ src/components/users/UsersOrganizer.tsx
@@ -12,64 +12,63 @@ const UsersOrganizer: React.FC = () => {
 
   const groupedUsersByDepartment = getUsersByDepartment();
 
-  const renderProcessedUsersByDepartment = () => {
-    return (
-      <div className="users-manager">
-        <div className="department-list">
-          {Object.entries(groupedUsersByDepartment).map(
-            ([department, users]) => (
-              <div key={department} className="department-section">
-                <h2>{`${department}: ${users.length}`}</h2>
-                {users.map((user: User) => (
-                  <div className="user-card" key={user.id}>
-                    <h3>{`${user.firstName} ${user.lastName}`}</h3>
-                    <p>Gender: {user.gender}</p>
-                    <p>Age: {user.age}</p>
-                    <p>Department: {user.company.department}</p>
-                  </div>
-                ))}
+  const renderUsersByDepartment = () => {
+    return Object.entries(
+      updateUsersByDepartment(groupedUsersByDepartment)
+    ).map(([department, values]) => (
+      <div key={department} className="department-section">
+        <h2>{`${department}`}:</h2>
+        <p>Males: {values.male}</p>
+        <p>Females: {values.female}</p>
+        <p>Ages between: {values.ageRange}</p>
+        <p>Hair Colors: </p>
+        {values.hair &&
+          Object.keys(values.hair).map((hairColor: string) => {
+            return (
+              <div key={hairColor}>
+                {hairColor}: {values.hair[hairColor]}
               </div>
-            )
-          )}
-        </div>
+            );
+          })}
       </div>
-    );
+    ));
   };
 
-  const renderUsersByDepartment = () => {
-    return (
-      <div className="users-manager">
-        <div className="department-list">
-          {Object.entries(
-            updateUsersByDepartment(groupedUsersByDepartment)
-          ).map(([department, values]) => (
-            <div key={department} className="department-section">
-              <h2>{`${department}`}:</h2>
-              <p>Males: {values.male}</p>
-              <p>Females: {values.female}</p>
-              <p>Ages between: {values.ageRange}</p>
+  const renderProcessedUsersByDepartment = () => {
+    return Object.entries(groupedUsersByDepartment).map(
+      ([department, users]) => (
+        <div key={department} className="department-section">
+          <h2>{`${department}: ${users.length}`}</h2>
+          {users.map((user: User) => (
+            <div className="user-card" key={user.id}>
+              <h3>{`${user.firstName} ${user.lastName}`}</h3>
+              <p>Gender: {user.gender}</p>
+              <p>Age: {user.age}</p>
+              <p>Department: {user.company.department}</p>
             </div>
           ))}
         </div>
-      </div>
+      )
     );
   };
 
   return (
-    <>
+    <div className="users-manager">
       <p className="process-data-button">
         <button
           onClick={() => {
             setShowProcessed((prev) => !prev);
           }}
         >
-          {showProcessed ? "Show By Department" : "Process Data"}
+          {!showProcessed ? "Show By Department" : "Process Data"}
         </button>
       </p>
-      {!showProcessed
-        ? renderUsersByDepartment()
-        : renderProcessedUsersByDepartment()}
-    </>
+      <div className="department-list">
+        {!showProcessed
+          ? renderUsersByDepartment()
+          : renderProcessedUsersByDepartment()}
+      </div>
+    </div>
   );
 };
 
